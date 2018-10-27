9301d40e47b2bff81ee6219a5a020ac64012b69f
Grace Choi <oiograceoio@gmail.com>
2018-10-27 05:02:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,9 +27,9 @@ class Song
 
   def self.genre_count
     genre_hash = {}
-    @@genres.each do |genre|
-      genre_hash[genre] = []
-      genre_hash[genre] << "1"
+    genre_hash.default = 1
+
+    @@genres.each do {|genre| genre_hash[genre] += 1}
     end
     genre_hash
   end
